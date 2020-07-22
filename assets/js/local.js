a1a37b6b3d00548746891a5c776b8efb0cb04ddf
theo-jaunet <theo.jaunet.sio@gmail.com>
2020-07-22 12:30:25 +0200

    update

--- assets/js/local.js
+++ assets/js/local.js
@@ -1,13 +1,17 @@
 const simW = [-6.5, 7]
 const simH = [-2.3, 6.1]
 
+let loc_xscacle
+let loc_yscacle
+
+
 function drawElements(data, svg) {
 
 
-    let xscale = d3.scaleLinear(simW, [10, 500]);
-    let yscale = d3.scaleLinear(simH, [10, 500]);
+     loc_xscacle = d3.scaleLinear(simW, [10, 500]);
+     loc_yscacle = d3.scaleLinear(simH, [10, 350]);
 
-    svg.selectAll(".locjDot")
+    svg.selectAll(".locDot")
         .data(data)
         .enter()
         .append("path")
@@ -15,8 +19,8 @@ function drawElements(data, svg) {
         .attr("id", d => d.id)
         .attr("d", "m 20 20 a -6 6 7 0 1 14 0 l -7 23 l -7 -23")
         .attr("transform", d => {
-            const orr = -90 + (Math.atan2(yscale(d.ly2) - yscale(d.ly1), xscale(d.lx2) - xscale(d.lx1)) * (180 / Math.PI));
-            return "translate(" + (xscale(d.lx1)) + " , " + (yscale(d.ly1)) + ") scale(0.5)  rotate(" + (orr) + ",4.5,6.25) "
+            const orr2 = -90 + (Math.atan2(proj_yscacle(d.ly2) - proj_yscacle(d.ly1), proj_xscacle(d.lx2) - proj_xscacle(d.lx1)) * (180 / Math.PI));
+            return "rotate(" + (orr2) + " " + (loc_xscacle(d.lx1) - 2) + " " + (loc_yscacle(d.ly1) - 2) + ") translate(" + (loc_xscacle(d.lx1) - 16) + "," + (loc_yscacle(d.ly1) - 16) + ")  scale(0.5) "
         })
 
 }
