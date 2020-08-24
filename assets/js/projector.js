8a5b2a175447221edd672b5e4cbe6ab228be0642
theo-jaunet <theo.jaunet.sio@gmail.com>
2020-08-24 19:03:38 +0200

    update

--- assets/js/projector.js
+++ assets/js/projector.js
@@ -2,8 +2,10 @@ function getRandomFloat(min, max) {
     return Math.random() * (max - min) + min;
 }
 
+/*
 let proj_xscacle
 let proj_yscacle
+*/
 
 
 function fakedata(npoints) {
@@ -37,13 +39,18 @@ function fakedata(npoints) {
 function drawPoints(data, svg) {
 
 
-    let p1 = [d3.extent(data.map(d => d.px1)), d3.extent(data.map(d => d.py1))];
-    let p2 = [d3.extent(data.map(d => d.px2)), d3.extent(data.map(d => d.py2))];
+    // let p1 = [d3.extent(data.map(d => d.px1)), d3.extent(data.map(d => d.py1))];
+    // let p2 = [d3.extent(data.map(d => d.px2)), d3.extent(data.map(d => d.py2))];
     // console.log([Math.min(p1[1], p2[1]), Math.max(p1[1], p2[1])]);
+    // let p1 = [[-5.714306831359863, 15.70352840423584],[-5.092494487762451, 14.56442642211914]];
+    // let p2 = p1
+    //
+    // console.log(p1)
+    // proj_xscacle = d3.scaleLinear([Math.min(p1[0][0], p2[0][0]), Math.max(p1[0][1], p2[0][1])], [15, 500]);
+    // proj_yscacle = d3.scaleLinear([Math.min(p1[1][0], p2[1][0]), Math.max(p1[1][1], p2[1][1])], [15, 500]);
 
-    console.log(p1)
-    proj_xscacle = d3.scaleLinear([Math.min(p1[0][0], p2[0][0]), Math.max(p1[0][1], p2[0][1])], [15, 500]);
-    proj_yscacle = d3.scaleLinear([Math.min(p1[1][0], p2[1][0]), Math.max(p1[1][1], p2[1][1])], [15, 500]);
+    // let proj_xscacle = d3.scaleLinear().domain(p1[0]).range([20, 515]); // Scale to Change based on SVG Size
+    // let proj_yscacle = d3.scaleLinear().domain(p1[1]).range([20, 515]);
 
     // console.log(d3.extent(data.map(d => d.py1)));
     // console.log(data.map(d => d.py1));
@@ -54,7 +61,7 @@ function drawPoints(data, svg) {
         .append("path")
         .attr("class", "projDot")
         .attr("num", d => d.id)
-        .attr("fill",(d) => colscale(euclidian_dist([proj_xscacle(d.px1),proj_yscacle(d.py1)],[proj_xscacle(d.px2),proj_yscacle(d.py2)])))
+        .attr("fill", (d) => colscale(euclidian_dist([proj_xscacle(d.px1), proj_yscacle(d.py1)], [proj_xscacle(d.px2), proj_yscacle(d.py2)])))
         .attr("d", "m 20 20 a -6 6 7 0 1 14 0 l -7 23 l -7 -23")
         .attr("transform", d => {
             const orr = -90 + (Math.atan2(proj_yscacle(d.py2) - proj_yscacle(d.py1), proj_xscacle(d.px2) - proj_xscacle(d.px1)) * (180 / Math.PI));
