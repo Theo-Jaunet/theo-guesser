d72dcc8edfcab399629e773f30c125474d4d7789
theo-jaunet <theo.jaunet.sio@gmail.com>
2020-08-25 13:25:40 +0200

    update

--- assets/js/local.js
+++ assets/js/local.js
@@ -14,6 +14,68 @@ d3.selection.prototype.moveToBack = function () {
     });
 };
 
+function drawElementsBad(data, svg) {
+
+
+    loc_xscacle = d3.scaleLinear(simW, [10, 500]);
+    loc_yscacle = d3.scaleLinear(simH, [10, 350]);
+
+
+    svg.selectAll(".locDot")
+        .data(data)
+        .enter()
+        .append("path")
+        .attr("class", "locDot")
+        .attr("id", d => d.id)
+        .attr("fill", (d) => colscaleizy(euclidian_dist([loc_xscacle(d.lx1), loc_yscacle(d.ly1)], [loc_xscacle(d.lx2), loc_yscacle(d.ly2)])))
+        .attr("d", "m 20 20 a -6 6 7 0 1 14 0 l -7 23 l -7 -23")
+        .attr("transform", d => {
+            const orr2 = 180 - d.lorr1 // + (Math.atan2(loc_yscacle(d.ly2) - loc_yscacle(d.ly1), loc_xscacle(d.lx2) - loc_xscacle(d.lx1)) * (180 / Math.PI));
+            return "rotate(" + (orr2) + " " + (loc_xscacle(d.lx1) - 2) + " " + (loc_yscacle(d.ly1) - 2) + ") translate(" + (loc_xscacle(d.lx1) - 16) + "," + (loc_yscacle(d.ly1) - 16) + ")  scale(0.5) "
+        })
+
+
+    /*
+            svg.selectAll(".locDot")
+            .data(data)
+            .enter()
+            .append("path")
+            .attr("class", "locDot")
+            .attr("id", d => d.id)
+            // .attr("fill",(d,i) => colscale(i))
+            .attr("d", "m 20 20 a -6 6 7 0 1 14 0 l -7 23 l -7 -23")
+            .attr("transform", d => {
+                const orr2 = -90 + d.torr
+                return "rotate(" + (orr2) + " " + (loc_xscacle(d.tx) - 2) + " " + (loc_yscacle(d.ty) - 2) + ") translate(" + (loc_xscacle(d.tx) - 16) + "," + (loc_yscacle(d.ty) - 16) + ")  scale(0.5) "
+            })
+    */
+
+
+    /*
+        let line = d3.line()
+            .x(function (d) {
+                return loc_xscacle(d.lx1)
+            })
+            .y(function (d) {
+                return loc_yscacle(d.ly1)
+            })
+                .curve(d3.curveLinear);
+
+
+        svg.append("path")
+            .data([data])
+            .attr("class", "traj")
+            .attr("d", line)
+            .attr('stroke-width', 3)
+            .attr('stroke', '#5b5b5b')
+            .attr('fill', 'none')
+    */
+
+
+    loadmap(svg, 'assets/images/map.jpg');
+
+}
+
 
 function drawElements(data, svg) {
 
@@ -28,48 +90,48 @@ function drawElements(data, svg) {
         .append("path")
         .attr("class", "locDot")
         .attr("id", d => d.id)
-        .attr("fill",(d) => colscale(euclidian_dist([loc_xscacle(d.lx1),loc_yscacle(d.ly1)],[loc_xscacle(d.lx2),loc_yscacle(d.ly2)])))
+        .attr("fill", (d) => colscale(euclidian_dist([loc_xscacle(d.lx1), loc_yscacle(d.ly1)], [loc_xscacle(d.lx2), loc_yscacle(d.ly2)])))
         .attr("d", "m 20 20 a -6 6 7 0 1 14 0 l -7 23 l -7 -23")
         .attr("transform", d => {
             const orr2 = -90 + (Math.atan2(loc_yscacle(d.ly2) - loc_yscacle(d.ly1), loc_xscacle(d.lx2) - loc_xscacle(d.lx1)) * (180 / Math.PI));
             return "rotate(" + (orr2) + " " + (loc_xscacle(d.lx1) - 2) + " " + (loc_yscacle(d.ly1) - 2) + ") translate(" + (loc_xscacle(d.lx1) - 16) + "," + (loc_yscacle(d.ly1) - 16) + ")  scale(0.5) "
         })
 
-/*
-        svg.selectAll(".locDot")
-        .data(data)
-        .enter()
-        .append("path")
-        .attr("class", "locDot")
-        .attr("id", d => d.id)
-        // .attr("fill",(d,i) => colscale(i))
-        .attr("d", "m 20 20 a -6 6 7 0 1 14 0 l -7 23 l -7 -23")
-        .attr("transform", d => {
-            const orr2 = -90 + d.torr
-            return "rotate(" + (orr2) + " " + (loc_xscacle(d.tx) - 2) + " " + (loc_yscacle(d.ty) - 2) + ") translate(" + (loc_xscacle(d.tx) - 16) + "," + (loc_yscacle(d.ty) - 16) + ")  scale(0.5) "
-        })
-*/
+    /*
+            svg.selectAll(".locDot")
+            .data(data)
+            .enter()
+            .append("path")
+            .attr("class", "locDot")
+            .attr("id", d => d.id)
+            // .attr("fill",(d,i) => colscale(i))
+            .attr("d", "m 20 20 a -6 6 7 0 1 14 0 l -7 23 l -7 -23")
+            .attr("transform", d => {
+                const orr2 = -90 + d.torr
+                return "rotate(" + (orr2) + " " + (loc_xscacle(d.tx) - 2) + " " + (loc_yscacle(d.ty) - 2) + ") translate(" + (loc_xscacle(d.tx) - 16) + "," + (loc_yscacle(d.ty) - 16) + ")  scale(0.5) "
+            })
+    */
 
 
+    /*
+        let line = d3.line()
+            .x(function (d) {
+                return loc_xscacle(d.lx1)
+            })
+            .y(function (d) {
+                return loc_yscacle(d.ly1)
+            })
+                .curve(d3.curveLinear);
 
 
-    let line = d3.line()
-        .x(function (d) {
-            return loc_xscacle(d.lx1)
-        })
-        .y(function (d) {
-            return loc_yscacle(d.ly1)
-        })
-            .curve(d3.curveLinear);
-
-
-    svg.append("path")
-        .data([data])
-        .attr("class", "traj")
-        .attr("d", line)
-        .attr('stroke-width', 3)
-        .attr('stroke', '#5b5b5b')
-        .attr('fill', 'none')
+        svg.append("path")
+            .data([data])
+            .attr("class", "traj")
+            .attr("d", line)
+            .attr('stroke-width', 3)
+            .attr('stroke', '#5b5b5b')
+            .attr('fill', 'none')
+    */
 
 
     loadmap(svg, 'assets/images/map.jpg');
